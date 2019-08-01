0da3c638c71f42072153ec2b682c5ff66f8871ed
agoulas42 <40356255+agoulas42@users.noreply.github.com>
2019-08-01 16:53:23 +0200

    Update javascript_client.js
    correction error copy

--- javascript_client.js
+++ javascript_client.js
@@ -1,55 +1,11 @@
-ar https = require('https');
-var request = require('request');
+var http = require('https');
+var server = http.createServer();
 
-/*
-** data needed for get the token
-*/
-var requestToken = {
-'username' : 'apitest001@clacdesdoigts.com',
-'password' : 'Asdfasdf00',
-'parameters': {
-    'SMS_IN': "false",
-    'CALL_IN': "true"
-}
-};
-/*
-// data needed for creation of new user
-var requestData = {
-'email' : 'apitest001@clacdesdoigts.com',
-'password' : 'Asdfasdf00',
-'firstName': 'Api',
-'lastName' : 'Test 041',
-'phoneNumber' : '+33799985881',
-'parameters': {
-    'SMS_IN': "false",
-    'CALL_IN': "true"
-}
-};
- */
-var options = {
-hostname: 'https://dev.api.clacdesdoigts.com/',
-path: 'v2/auth/token/validate',
-method: 'POST',
-headers: {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer <jwt_token>',
-    'Accept-Language' : 'en'
-}
-};
- 
-var req = https.request(options, function(res) {
-	var body =  new Buffer(0);
-	res.on('data',function (chunk)
-	{
-		body =  Buffer.concat([body,chunk],body.length+chunk.length);
-	});
-	res.once('end', function ()
-	{
-		 console.log(body.toString('utf8'));
-	});
+server.on("request", function(request,response) {
+    request.on("data",function(chunk){
+        console.log("le serveur a recu :", + chunk); 
+    }); 
+    response.end();
 });
 
-req.end(new Buffer(JSON.stringify(requestToken)));
-req.on('error', function(e) {
-console.error(e);
-});
+server.listen(3000);
